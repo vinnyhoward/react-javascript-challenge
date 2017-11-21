adbbb6d40ba9453149281962d0f8dd2573a07ac3
Vincent Howard <vincenguyenhoward@gmail.com>
2017-11-21 11:35:47 -0700

    finished app1

---
+++ app1-describe-age/src/components/input.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class Input extends Component {
+constructor(props) {
+  super(props)
+  this.state={
+    value: '',
+    age: ''
+  }
+  this.handleChange = this.handleChange.bind(this);
+  this.describeAge = this.describeAge.bind(this);
+}
+
+
+handleChange(event) {
+  this.setState({
+    value: event.target.value
+  })
+  console.log(event.target.value);
+}
+
+describeAge(num) {
+  let howOld = num <= 12 ? "kid" 
+  : num >= 13 && num <= 17 ? "teenager" 
+  : num >= 18 && num <= 64 ? "adult"
+  : "elderly" 
+  this.setState({
+    age: howOld
+  })
+  console.log("You're a(n) " + howOld);
+}
+
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.value} onChange={ this.handleChange } /> 
+        <button className="button-thing" onClick={ () => this.describeAge( this.state.value ) }>Check Console</button>
+      </div>
+    )
+  }
+}
