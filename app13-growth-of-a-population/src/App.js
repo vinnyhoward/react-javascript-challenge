255febd3434f7e14ea05aebacb4b18aa24efe7d6
Vincent Howard <vincenguyenhoward@gmail.com>
2017-12-02 15:47:24 -0700

    finished react portion for app13

--- app13-growth-of-a-population/src/App.js
+++ app13-growth-of-a-population/src/App.js
@@ -3,16 +3,105 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      input: '',
+      starting: [],
+      percent: [],
+      aug: [],
+      targetPop: [],
+      years: [],
+    }
+    this.handleChangeStarting = this.handleChangeStarting.bind(this)
+    this.handlePercent = this.handlePercent.bind(this)
+    this.handleAug = this.handleAug.bind(this)
+    this.handleTargetPop = this.handleTargetPop.bind(this)
+    this.populationGrowth = this.populationGrowth.bind(this)
+  }
+handleChangeStarting(event) {
+this.setState({
+  starting: event.target.value
+})
+}
+
+handlePercent(event) {
+  this.setState({
+    percent: event.target.value
+  })
+}
+
+handleAug(event) {
+  this.setState({
+    aug: event.target.value
+  })
+}
+
+handleTargetPop(event) {
+  this.setState({
+    targetPop: event.target.value
+  })
+}
+
+populationGrowth(p0, percent, aug, p){
+  var years = 1;
+  var i = p0 + (p0 * (percent * 0.01)) + aug
+  for( i; i < p; i++) {
+    i = i + (i * (percent * 0.01)) + aug
+    years++
+  }
+  var newYears = years
+  this.setState({
+    years: newYears
+  })
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Target Population</h1>
+          <p className="sub-title">Find out how many years till you get your target population</p>
+          <h3 className="content">{this.state.years}</h3>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div className='input-wrapper'>
+       <input 
+        type="text" 
+        value={this.state.value} 
+        onChange={ this.handleChangeStarting }
+        className='input-field'
+        placeholder='Starting Population'
+         /> 
+        <input 
+        type="text" 
+        value={this.state.value} 
+        onChange={ this.handlePercent }
+        className='input-field'
+        placeholder='Growth By Percent'
+         /> 
+        <input 
+        type="text" 
+        value={this.state.value} 
+        onChange={ this.handleAug }
+        className='input-field'
+        placeholder='Leaving/Entering Population'
+         /> 
+        <input 
+        type="text" 
+        value={this.state.value} 
+        onChange={ this.handleTargetPop }
+        className='input-field'
+        placeholder='Desired Population'
+         /> 
+        <button 
+        className="button-thing"
+        onClick={() => this.populationGrowth(this.state.starting, 
+          this.state.percent,  
+          this.state.aug,
+          this.state.targetPop)}
+         >YEARS TILL TARGET</button>
+       </div>
       </div>
     );
   }
