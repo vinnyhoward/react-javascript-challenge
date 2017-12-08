84f04034d9b842142ed51b7091aa027c0635e0f2
Vincent Howard <vincenguyenhoward@gmail.com>
2017-12-07 22:49:40 -0700

    finished app14 react portion

--- week-2/app14-return-even-string/src/App.js
+++ week-2/app14-return-even-string/src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      value: '',
+      letters: [],
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.sortByEven.bind = this.sortByEven.bind(this)
+}
+
+handleChange(event) {
+  this.setState({
+    value: event.target.value
+  })
+}
+
+
+sortByEven(string){
+  var splitStr = string.split('')
+  var newStringArr = []
+  if(string.length < 2 || string.length > 100) {
+    return "invalid string"
+  } else {
+  for(var i = 0; i < splitStr.length; i++) {
+    if(i % 2 !== 0) {
+      newStringArr.push(splitStr[i] + ' ')
+    }
+  }
+  }
+  this.setState({
+    letters: newStringArr
+  })
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Return Even String</h1>
+          <p className="title">{this.state.letters}</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='content-wrapper'>
+       <input 
+       type="text" 
+       onChange={this.handleChange} 
+       className='main-input' 
+       placeholder='Enter Letters Here'
+       />
+       <button 
+       onClick={() => this.sortByEven(this.state.value)}
+       className='main-button'
+       >SORT LETTERS BY NUMBER</button>
+        </div>
       </div>
     );
   }
