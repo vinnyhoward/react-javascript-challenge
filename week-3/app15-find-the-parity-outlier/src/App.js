99c7b32091d58f6d39a24c3a3d9623cf184658c1
Vincent Howard <vincenguyenhoward@gmail.com>
2017-12-07 23:39:27 -0700

    Finished app16 react portion

--- week-3/app15-find-the-parity-outlier/src/App.js
+++ week-3/app15-find-the-parity-outlier/src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      value: '',
+      number: [],
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.sortNumber.bind = this.sortNumber.bind(this)
+}
+
+handleChange(event) {
+  this.setState({
+    value: event.target.value
+  })
+}
+
+
+sortNumber(integers){
+  var evenNum = []
+  var oddNum = []
+  for(var i = 0; i < integers.length; i++) {
+    if( integers[i] % 2 !== 0 ) {
+      oddNum.push(integers[i])
+    }
+  }
+  for(var j = 0; j < integers.length; j++) {
+    if( integers[j] % 2 === 0 ) {
+      evenNum.push(integers[j])
+    }
+  }
+  if ( evenNum.length < oddNum.length) {
+    this.setState({
+      number: evenNum[0]
+    }) 
+  } else {
+    this.setState({
+      number: oddNum[0]
+    }) 
+  }
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Return Even String</h1>
+          <p className="title">{this.state.number}</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='content-wrapper'>
+       <input 
+       type="text" 
+       onChange={this.handleChange} 
+       className='main-input' 
+       placeholder='Enter Numbers Here'
+       />
+       <button 
+       onClick={() => this.sortNumber(this.state.value)}
+       className='main-button'
+       >FIND OUTLIER</button>
+        </div>
       </div>
     );
   }
