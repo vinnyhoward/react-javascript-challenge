d86ae8cdeb16e44700a31c6802f4bc193e243d12
Vincent Howard <vincenguyenhoward@gmail.com>
2017-12-14 17:14:31 -0700

    🎉Finished react portion of app17

--- week-3/app17-most-digits/src/App.js
+++ week-3/app17-most-digits/src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      value: '',
+      digits: [],
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.findMostDigits.bind = this.findMostDigits.bind(this)
+}
+
+handleChange(event) {
+  this.setState({
+    value: event.target.value
+  })
+}
+
+findMostDigits = array => {
+  var biggestNumber = '';
+  var newNum = array.toString().split(',')
+  for(var i = 0; i< newNum.length; i++){
+      if(newNum[i].length > biggestNumber.length){
+          biggestNumber = newNum[i];
+       }
+  }
+  this.setState({
+    digits: parseInt(biggestNumber)
+  })
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Most Digits</h1>
+          <p className="title">{this.state.digits}</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='content-wrapper'>
+       <input 
+       type="text" 
+       onChange={this.handleChange} 
+       className='main-input' 
+       placeholder='Enter Numbers Here'
+       />
+       <button 
+       onClick={() => this.findMostDigits(this.state.value)}
+       className='main-button'
+       >Find</button>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
