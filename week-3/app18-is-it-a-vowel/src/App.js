5b8705b72929de1f404d4460282598bdbfebd10a
Vincent Howard <vincenguyenhoward@gmail.com>
2017-12-14 17:54:46 -0700

    🎉Finished react portion of app18

--- week-3/app18-is-it-a-vowel/src/App.js
+++ week-3/app18-is-it-a-vowel/src/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      value: '',
+      vowel: null,
+      number: [],
+      word: [],
+    }
+    this.handleChangeValue = this.handleChangeValue.bind(this)
+    this.handleChangeNumber = this.handleChangeNumber.bind(this)
+    this.checkVowel.bind = this.checkVowel.bind(this)
+}
+handleChangeValue(event) {
+  this.setState({
+    word: event.target.value
+  })
+}
+handleChangeNumber(event) {
+  this.setState({
+    number: event.target.value
+  })
+}
+checkVowel=(s,p)=> {
+const filteredVowels = 'aeiouAEIOU'.includes(s[p])
+  this.setState({
+    vowel: filteredVowels
+  })
+  console.log(filteredVowels);
+}
+
+vowelChecked(){
+  if(this.state.vowel === true) {
+    return <div>True</div>
+  } else if(this.state.vowel === false) {
+    return <div>False</div>
+  }
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Most Digits</h1>
+          <div className="title">{this.vowelChecked()}</div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='content-wrapper'>
+       <input 
+       type="text" 
+       onChange={this.handleChangeValue} 
+       className='main-input' 
+       placeholder='Enter Words Here'
+       />
+          <input 
+       type="text" 
+       onChange={this.handleChangeNumber} 
+       className='main-input' 
+       placeholder='Enter Numbers Here'
+       />
+       <button 
+       onClick={() => this.checkVowel(this.state.word, this.state.number)}
+       className='main-button'
+       >Find Vowel</button>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
