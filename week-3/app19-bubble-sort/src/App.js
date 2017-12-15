587317b0ce24ddcc83b382c5628346e311d5d268
Vincent Howard <vincenguyenhoward@gmail.com>
2017-12-14 18:06:26 -0700

    🎉Finished react portion of app19

--- week-3/app19-bubble-sort/src/App.js
+++ week-3/app19-bubble-sort/src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      value: [9,7,5,3,1,2,4,6,8],
+      sortNum: [9,7,5,3,1,2,4,6,8],
+    }
+    this.handleChangeValue = this.handleChangeValue.bind(this)
+    this.bubbleSortNum.bind = this.bubbleSortNum.bind(this)
+}
+
+handleChangeValue(event) {
+  this.setState({
+    value: event.target.value
+  })
+}
+
+bubbleSortNum = (intArr) => {
+  for(var i = 0; i < intArr.length; i++){
+    if(intArr[i] > intArr[i + 1]){
+      let temp = intArr[i]
+     intArr[i] = intArr[i+ 1]
+     intArr[i+ 1] = temp
+   }
+ }
+ this.setState({
+  sortNum: intArr
+})
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Most Digits</h1>
+          <div className="title">{this.state.sortNum}</div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='content-wrapper'>
+       <input 
+       type="text" 
+       onChange={this.handleChangeValue} 
+       className='main-input' 
+       placeholder='Enter New Numbers Here'
+       />
+       <button 
+       onClick={() => this.bubbleSortNum(this.state.value)}
+       className='main-button'
+       >Bubble Sort</button>
+        </div>
       </div>
     );
   }
---
+++ week-3/app19-bubble-sort/toyProblem.js
@@ -0,0 +1,10 @@
+let bubblesortOnce = intArr => {
+  for(var i = 0; i < intArr.length; i++){
+     if(intArr[i] > intArr[i + 1]){
+       let temp = intArr[i]
+      intArr[i] = intArr[i+ 1]
+      intArr[i+ 1] = temp
+    }
+  }
+  return intArr
+}
